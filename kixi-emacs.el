80e023e540a14cfd583b78665748b38577cd0180
Bruce Durling <bruce@mastodonc.com>
2022-07-19 09:26:36 +0100

    Add more cleverparens hooks

--- kixi-emacs.el
+++ kixi-emacs.el
@@ -320,6 +320,8 @@
 ;; evil cleverparens
 (straight-use-package 'evil-cleverparens)
 (add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
+(add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
+(add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
 
 (provide 'kixi-emacs)
 
