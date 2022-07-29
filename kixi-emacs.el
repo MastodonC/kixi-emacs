60764af0e0328c99c2a793b30526cdce575c77e7
Bruce Durling <bruce@mastodonc.com>
2022-07-29 13:41:29 +0100

    Give a hoot. Don't pollute.
    Fix for #9

--- kixi-emacs.el
+++ kixi-emacs.el
@@ -75,6 +75,16 @@
 (customize-set-variable 'savehist-file
                         (expand-file-name ".cache/history" user-emacs-directory))
 
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
+
+
 ;; (provide 'kixi-basics)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
